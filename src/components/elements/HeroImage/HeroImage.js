2a3d65e3409a41bb42864161bf6db8db2d337668
BastienRoques <bastien.roques971@gmail.com>
2019-06-02 13:20:08 +0200

    get API data & add HeroImage

--- src/components/elements/HeroImage/HeroImage.js
+++ src/components/elements/HeroImage/HeroImage.js
@@ -3,8 +3,18 @@ import './HeroImage.css';
 
 const HeroImage = (props) => {
     return(
-        <div>
-            HeroImage
+        <div 
+            className="rmdb-heroimage"
+            style={{
+                background: `url('${props.image}'), #1C1C1C`
+            }}
+        >
+            <div className="rmdb-heroimage-content">
+                <div className="rmdb-heroimage-text">
+                    <h1>{props.title}</h1>
+                    <p>{props.text}</p>
+                </div>
+            </div>
         </div>
     );
 }
