83d2859181a020587dc8a3c2afbd0d9b0b7447e8
BastienRoques <bastien.roques971@gmail.com>
2019-06-04 14:58:55 +0200

    add components for the movie page

---
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FontAwesome from '@fortawesome/react-fontawesome';
+import { calcTime, convertmoney } from '../../../helpers.js';
+
+import './MovieInfoBar.css';
+
+const MovieInfoBar = (props) => {
+    return(
+        <div>
+            MovieInfoBar
+        </div>
+    );
+}
+
+export default MovieInfoBar;
