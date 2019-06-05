645347597c478e1b88eb6debfb3691ed4ff823f6
BastienRoques <bastien.roques971@gmail.com>
2019-06-05 11:09:02 +0200

    add Local Storage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,9 +19,14 @@ class Home extends Component {
     }
 
     componentDidMount() {
-        this.setState({ loading: true });
-        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=fr-FR&page=1`;
-        this.fetchItems(endpoint);
+        if(localStorage.getItem('HomeState')) {
+            const state = JSON.parse(localStorage.getItem('HomeState'));
+            this.setState({ ...state });
+        } else {
+            this.setState({ loading: true });
+            const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=fr-FR&page=1`;
+            this.fetchItems(endpoint);
+        }
     }
 
     searchItems =(searchTerm) => {
@@ -63,6 +68,10 @@ class Home extends Component {
                 loading: false,
                 currentPage: result.page,
                 totalPages: result.total_pages 
+            }, () => {
+                if(this.state.searchTerm === "") {
+                    localStorage.setItem('HomeState', JSON.stringify(this.state));
+                }
             });
         });
     }
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -18,10 +18,15 @@ class Movie extends Component {
     }
 
     componentDidMount() {
-        this.setState({ loading: true })
-        // Récupération des infos du film ...
-        const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=fr-FR`;
-        this.fetchItems(endpoint);
+        if(localStorage.getItem(`${this.props.match.params.movieId}`)) {
+            const state = JSON.parse(localStorage.getItem(`${this.props.match.params.movieId}`));
+            this.setState({ ...state });
+        } else {
+            this.setState({ loading: true })
+            // Récupération des infos du film ...
+            const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=fr-FR`;
+            this.fetchItems(endpoint);
+        }
     }
 
     fetchItems = (endpoint) => {
@@ -44,6 +49,8 @@ class Movie extends Component {
                             actors: result.cast,
                             directors,
                             loading: false
+                        }, () => {
+                            localStorage.setItem(`${this.props.match.params.movieId}`, JSON.stringify(this.state));
                         });
                     });
                 });
